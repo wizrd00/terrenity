40389424c6886841166a19d21dabd61af38158da
wizrd <wizrd.bin@proton.me>
2026-03-01 14:40:13 +0330

    remove BLINK

--- test/test_shadowed_button/test.c
+++ test/test_shadowed_button/test.c
@@ -1,6 +1,8 @@
 #include "unity.h"
 #include "terrenity.h"
 
+matrix_t mx;
+
 void setUp(void)
 {
 	struct winsize ws;
@@ -9,6 +11,7 @@ void setUp(void)
 	mx.col = ws.ws_col;
 	if (mx_init(&mx, true, true) != SUCCESS)
 		fprintf(stderr, "mx_init() failed");
+	mx_clear();
 	return;
 }
 
@@ -17,23 +20,80 @@ void tearDown(void)
 	mx_deinit(&mx);
 	return;
 }
-
-void test_fill_screen(void)
+void test(void)
 {
 	status_t _stat = SUCCESS;
+	object_t sun = {
+		.shape = RECTANGLE,
+		.pixel = {
+			.ulbd = BLINK,
+			.bgnd = HBGYELLOW,
+			.fgnd = LFGYELLOW,
+			.cval = '~'
+		},
+		.active = true,
+		.fill = true,
+		.x = mx.col - 10,
+		.y = 2,
+		.len = 8,
+		.wid = 4
+	};
+	object_t shadow = {
+		.shape = RECTANGLE,
+		.pixel = {
+			.ulbd = NONE,
+			.bgnd = LBGBLACK,
+			.fgnd = LFGBLACK,
+			.cval = ' '
+		},
+		.active = true,
+		.fill = true,
+		.x = mx.col / 2 - 10,
+		.y = mx.row / 2 - 4,
+		.len = 10,
+		.wid = 4
+	};
+	object_t button = {
+		.shape = RECTANGLE,
+		.pixel = {
+			.ulbd = NONE,
+			.bgnd = LBGWHITE,
+			.fgnd = LFGWHITE,
+			.cval = ' '
+		},
+		.active = true,
+		.fill = true,
+		.x = mx.col / 2 - 9,
+		.y = mx.row / 2 - 5,
+		.len = 10,
+		.wid = 4
+	};
+	object_t *handle0;
+	object_t *handle1;
+	object_t *handle2;
+
 	_stat = mx_fill(&mx, &(pixel_t){.ulbd = NONE, .bgnd = HBGBLUE, .fgnd = HFGBLUE, .cval = ' '});
 	TEST_ASSERT_EQUAL(SUCCESS, _stat);
 
 	_stat = mx_refresh(&mx);
 	TEST_ASSERT_EQUAL(SUCCESS, _stat);
-}
 
-void test_button(void)
-{
+	_stat = mx_popup(&mx, &sun, &handle0);
+	TEST_ASSERT_EQUAL(SUCCESS, _stat);
 
+	_stat = mx_popup(&mx, &shadow, &handle1);
+	TEST_ASSERT_EQUAL(SUCCESS, _stat);
+
+	_stat = mx_popup(&mx, &button, &handle2);
+	TEST_ASSERT_EQUAL(SUCCESS, _stat);
+
+	_stat = mx_render(&mx);
+	TEST_ASSERT_EQUAL(SUCCESS, _stat);
+	return;
 }
 
 int main(void) {
 	UNITY_BEGIN();
+	RUN_TEST(test);
 	return UNITY_END();
 }
